1961654d88cddf0c0a45edd33d6ab86d87b0f94f
Salika155 <62901117+Salika155@users.noreply.github.com>
2022-11-23 19:07:01 +0100

    ew
    we

--- Daniel_Armero_Carlos_Carretero_Practica/Daniel_Armero_Carlos_Carretero_Practica/Form1.cs
+++ Daniel_Armero_Carlos_Carretero_Practica/Daniel_Armero_Carlos_Carretero_Practica/Form1.cs
@@ -6,5 +6,15 @@ namespace Daniel_Armero_Carlos_Carretero_Practica
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
