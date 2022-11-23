eb81a9e6e02ef5892628339cc4dade3492896b89
Salika155 <62901117+Salika155@users.noreply.github.com>
2022-11-23 18:29:33 +0100

    commit

---
+++ Daniel_Armero_Carlos_Carretero_Practica/Daniel_Armero_Carlos_Carretero_Practica/Form1.cs
@@ -0,0 +1,10 @@
+namespace Daniel_Armero_Carlos_Carretero_Practica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
