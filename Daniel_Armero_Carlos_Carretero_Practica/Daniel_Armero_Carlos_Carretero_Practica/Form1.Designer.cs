7554fe0c7b66e49910ce72d2d1b2374140c7056e
Salika155 <carre155@hotmail.com>
2022-11-24 12:09:11 +0100

    Segunda versión: añado código con errores
    commit

--- Daniel_Armero_Carlos_Carretero_Practica/Daniel_Armero_Carlos_Carretero_Practica/Form1.Designer.cs
+++ Daniel_Armero_Carlos_Carretero_Practica/Daniel_Armero_Carlos_Carretero_Practica/Form1.Designer.cs
@@ -49,7 +49,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(596, 355);
+            this.button1.Location = new System.Drawing.Point(578, 355);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 36);
             this.button1.TabIndex = 1;
